479db644e885344f42e0a2ab164297506a7e3796
xujie-nm <xujie_nm@163.com>
2015-10-12 21:46:49 +0800

    151012

--- Chapter1/Factory/Factory.cpp
+++ Chapter1/Factory/Factory.cpp
@@ -52,6 +52,7 @@ namespace version1{
 }
 
 namespace version2{
+    //数据成员修改为std::map<string, weak_ptr<Stock> > stocks_;
 	class StockFactory : boost::noncopyable
 	{
 	public:
@@ -59,10 +60,13 @@ namespace version2{
 			boost::shared_ptr<Stock> pStock;
 			muduo::MutexLockGuard lock(mutex_);
 			boost::weak_ptr<Stock>& wkStock = stocks_[key];
+            //如果key不存在，会默认构造一个
 			pStock = wkStock.lock();
+            //尝试提升weak_ptr
 			if(!pStock){
 				pStock.reset(new Stock(key));
 				wkStock = pStock;
+                //这里更新了stocks_[key],注意wkStock是一个引用
 			}
 			return pStock;
 		}
@@ -82,6 +86,7 @@ namespace version3{
 			boost::weak_ptr<Stock> &wkStock = stocks_[key];
 			pStock = wkStock.lock();
 			if(!pStock){
+                //传入一个函数用来删除stock
 				pStock.reset(new Stock(key), boost::bind(&StockFactory::deleteStock, this, _1));
 				wkStock = pStock;
 			}
@@ -89,6 +94,7 @@ namespace version3{
 		}
 	private:
 
+        //定义删除的函数
 		void deleteStock(Stock * stock){
 			printf("deleteStock[%p]\n", stock);
 			if(stock){
@@ -103,6 +109,7 @@ namespace version3{
 }
 
 namespace version4{
+    //这里继承的这个类是为了让this指针变为shared_ptr
 	class StockFactory : public boost::enable_shared_from_this<StockFactory>,
 								       boost::noncopyable
 	{
@@ -141,11 +148,14 @@ public:
 		boost::shared_ptr<Stock> pStock;
 		muduo::MutexLockGuard lock(mutex_);
 		boost::weak_ptr<Stock> &wkStock = stocks_[key];
+        // 注意wkStock是引用
 		pStock = wkStock.lock();
 		if(!pStock){
 			pStock.reset(new Stock(key), 
 				boost::bind(&StockFactory::weakDeleteCallback, boost::weak_ptr<StockFactory>(shared_from_this()),
 				_1));
+            // 上面必须强制吧shared_from_this转型为weak_ptr，才不会延长生命周期（shared_ptr是铁丝，weak_ptr是棉线）
+            // 因为boost::bind拷贝的是实参类型，不是形参类型
 			wkStock = pStock;
 		}
 		return pStock;
@@ -156,7 +166,9 @@ private:
 									Stock *stock){
 		printf("weakDeleteStock[%p]\n", stock);
 		boost::shared_ptr<StockFactory> factory(wkFactory.lock());
+        // 尝试提升
 		if(factory){
+            //如果还在，就进行清理
 			factory->removeStock(stock);
 		}else{
 			printf("factory died.\n");
@@ -204,7 +216,9 @@ int main(int argc, char const *argv[])
 	version1::StockFactory sf1;
 	version2::StockFactory sf2;
 	version3::StockFactory sf3;
-	boost::shared_ptr<version3::StockFactory> sf4(new version3::StockFactory);
+	boost::shared_ptr<version4::StockFactory> sf4(new version4::StockFactory);
+    //这么命名对象是因为，StockFactory继承了enable_shared_from_this的缘故
+    //StockFactory不能是stack object，必须是heap_object
 	boost::shared_ptr<StockFactory> sf5(new StockFactory);
 
 	{
